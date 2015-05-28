76f6d857b9783b0d82cef09a7e244a263596eb9b
smilne121 <smilne@waitaki.govt.nz>
2015-05-28 18:24:23 +1200

    Created home screen and added default objects to page

---
+++ BuildingWaitaki/BuildingWaitaki/HomeController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuildingWaitaki
+//
+//  Created by Scott Milne on 28/05/15.
+//  Copyright (c) 2015 Scott Milne. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
