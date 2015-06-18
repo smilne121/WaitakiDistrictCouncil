6f5940fae9f491ceaf7b316a1ac68236168bfcaf
smilne121 <smilne@waitaki.govt.nz>
2015-06-18 22:58:11 +1200

    Items showing on screen with bool options available

--- BuildingWaitaki/BuildingWaitaki/CurrentInspectionViewController.swift
+++ BuildingWaitaki/BuildingWaitaki/CurrentInspectionViewController.swift
@@ -54,8 +54,71 @@ class CurrentInspectionViewController: UIViewController {
     
     func generateItemsOnscreen()
     {
+        var leftSide = Bool(true)
+        let fontsize = CGFloat(15)
+        var currentX = 0
+        var currentY = 0
+        let height = 200
+        let width = Int(itemHolder.frame.width / 2 )
         
+        //loop though items and create containers
+        for item in inspectionTypeItems
+        {
+            if item.itemName != "Complete"
+            {
+            let containerRect: CGRect = CGRect(x: currentX,y: currentY,width: width,height: height)
+            let container: UIView = UIView(frame: containerRect)
+            container.layer.borderColor = UIColor .blackColor().CGColor
+            container.layer.borderWidth = 1.0
+            container.backgroundColor = UIColor.whiteColor()
+            container.tintColor = UIColor.blackColor()
+            
+            
+            // add label to item
+            let itemName = UILabel(frame: CGRect(x: 5, y: 5, width: container.frame.width, height: 30))
+            itemName.text = item.itemName
+            let font = UIFont(name: itemName.font.fontName, size: CGFloat(20))
+            itemName.font = font
+            container.addSubview(itemName)
+            itemHolder.addSubview(container)
+            
+            //check item type and add selector
+            if item.itemType.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) == "F" //Flag type = bool
+            {
+                let selectorItems = ["Passed" ,"Failed"]
+                let selector = UISegmentedControl(items: selectorItems)
+                selector.selectedSegmentIndex = -1
+                selector.tintColor = UIColor.darkGrayColor()
+                
+                var attr = NSDictionary(object: UIFont(name: "HelveticaNeue-Bold", size: 16.0)!, forKey: NSFontAttributeName)
+                selector.setTitleTextAttributes(attr as [NSObject : AnyObject], forState: .Normal)
+                
+                selector.frame = CGRect(x: 10, y: 50, width: container.frame.width - 20, height: 80)
+                container.addSubview(selector)
+                
+            }
+            
+            //move to next space
+            if leftSide == true
+            {
+                leftSide = false
+                currentX = width + currentX
+            }
+            else
+            {
+                currentX = width - currentX
+                currentY = height + currentY
+                leftSide = true
+            }
+            }
+        }
+        
+        //update content size of scrollview to match
+        let contentSize = CGSize(width: itemHolder.frame.width, height: CGFloat(currentY) + CGFloat(height) )
+        itemHolder.contentSize = contentSize
     }
+    
+
     
 
     /*
